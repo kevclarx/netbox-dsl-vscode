094881f1f3945d9b6f0a89546bc21721dfbc0cd9
Jermaine Tate <tate.jermaine@gmail.com>
2023-06-15 19:07:44 -0600

    views for device, racks, sites

--- media/main.js
+++ media/main.js
@@ -13,16 +13,66 @@
     // Handle messages sent from the extension to the webview
     window.addEventListener('message', event => {
         const message = event.data; // The json data that the extension sent
+        const props = JSON.parse(message.props);
+        const devices = message.devices;
+        console.log("props: ",props);
+        console.log("data: ",message.data)
 
-        console.log("These are my devices: ",message.data);
+        var labweekHeader = document.getElementById('lwheader');
+        labweekHeader.textContent = `${props['name']}`
 
+        // Create table.
         var table = document.createElement("table");
-        for (var VAL of message.devices) { 
-            var row = table.insertRow();
-            let cell = row.insertCell();
-            cell.innerHTML = VAL; 
+
+        for(const key in props){
+          if (key.startsWith('_')){
+            continue
+          }
+          var treeRow = table.insertRow();
+          var treeCell = treeRow.insertCell();
+          treeCell.innerHTML = key;
+          var treeCell2 = treeRow.insertCell();
+          treeCell2.innerHTML = props[key];
+        };
+        document.getElementById("treeone").appendChild(table);
+
+        switch (message.data['type']) {
+            case 'site':
+
+                break;
+
+            case 'rack':
+              
+                
+                // second table to populate
+                var treeTable = document.createElement("table");
+                const numOfRacksUnits = Number(props["ru"]);
+  
+
+                for (let i = numOfRacksUnits; i >= 1; i--) {
+                    var treeRow = treeTable.insertRow();
+                    var treeCell = treeRow.insertCell();
+                    treeCell.innerHTML = i;
+                    console.log("rack unit i", i);
+                    for (let j = 0; j < devices.length; j++ )
+                    {
+                      console.log("Devices j:", devices[j])
+                      if (devices[j].position == i){
+                        var treeCell2 = treeRow.insertCell();
+                        treeCell2.innerHTML = devices[j].name;
+
+                      }
+                    }
+                }
+                document.getElementById("rackview").appendChild(treeTable);
+
+                break;
+            
+            case 'device':
+
+              break;
+
         }
-        document.getElementById("treeview").appendChild(table);
 
 
     });
--- src/webview.ts
+++ src/webview.ts
@@ -3,89 +3,46 @@ import * as vscode from 'vscode';
 
     export function getWebviewContent(styleMainUri: vscode.Uri, scriptUri: vscode.Uri) {
 		return `<!DOCTYPE html>
-				<html lang="en">
-				<head>
-					<meta charset="UTF-8">
-					<meta name="viewport" content="width=device-width, initial-scale=1.0">
-					<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-					<link href="${styleMainUri}" rel="stylesheet">
-				</head>
-				<body>
-					<!-- <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" /> -->
-					
-					
-				<!--	<section>
-					<nav>
-					</nav>
-					<article>
-						
-					</article>
-					</section> -->
+		<html>
+		<head>
+		  <style>
+			.container {
+			  display: flex;
+			  height: 100vh;
+			  overflow: auto;
+			}
+			.table {
+			  border-collapse: collapse;
+			  border: 1px solid black;
+			  width: 50%;
+			}
+			.table2 {
+			  border-collapse: collapse;
+			  border: 1px solid black;
+			  width: 33%;
+			  counter-reset: rowNumber;
+			}
+			.table2 tr td:first-child::before {
+			  min-width: 1em;
+			  margin-right: 0.5em;
+			}
+			.space {
+			  width: 20%;
+			}
+		  </style>
+		</head>
+		<body>
+		<h2 class="align-items-center" id="lwheader"></h2>
+		  <div class="container">
+			<table class="table" id="treeone">
 
-					<!--	<div class="container mt-5">
-						<div class="row">
-
-						  <div class="col">
-						  <table>
-						  <tr>
-							  <th>Site</th>
-							  <td>Detroit</td>
-						  </tr>
-						  <tr>
-							  <th>Region</th>
-							  <td>Central</td>
-						  </tr>
-						  <tr>
-							  <th>RU</th>  
-							  <td>24</td>
-						  </tr>
-						  <tr>
-							  <th>Height</th>  
-							  <td>48</td>
-						  </tr>
-						</table>
-
-						  </div>
-						  <div class="col">
-						  <table id="treeview"></table>
-						  </div>
-						</div>
-					  </div> -->
-
-
-					<div>
-						<div class="row " style="height: 100vh;"> <!-- align-items-center -->
-							<div class="col-8"> <!-- class="mx-auto col-10 col-md-8 col-lg-6" -->
-								<table class="table">
-								<tr>
-									<th>Site</th>
-									<td>Detroit</td>
-								</tr>
-								<tr>
-									<th>Region</th>
-									<td>Central</td>
-								</tr>
-								<tr>
-									<th>RU</th>  
-									<td>24</td>
-								</tr>
-								<tr>
-									<th>Height</th>  
-									<td>48</td>
-								</tr>
-								</table>
-							</div>
-							<div class="col-4">
-							<table class="table table-bordered table-dark" id="treeview"></table>
-							</div>
-						</div>
-					</div>
-					
-
-						
-	<!-- Bootstrap Bundle with Popper -->
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-					<script src="${scriptUri}"></script>
-				</body>
-				</html>`;
+			</table>
+			<div class="space"></div>
+			<table class="table2" id="rackview">
+			
+			</table>
+		  </div>
+		  <script src="${scriptUri}"></script>
+		</body>
+		</html>`;
 	}
